75a2d517423831f62b2ea985e340c9e0268414d8
Juvenal Guzman <juvenal.guzman@tenser.mx>
2018-09-12 15:11:11 -0500

    Initial commit

---
+++ examples/usage/usage.ino
@@ -0,0 +1,14 @@
+// Example usage for SofAPLib library by Juvenal Guzman.
+
+#include "SoftAPLib.h"
+
+// Initialize objects from the lib
+STARTUP(softap_set_application_page_handler(SoftAPLib::getPage, nullptr));
+
+void setup() {
+
+}
+
+void loop() {
+
+}
